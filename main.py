2d09af642fba097a004a35d596d38086ba45dbe3
cui1fan <1819285039@qq.com>
2025-09-14 17:25:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,9 @@
-def syetem_huiwen(number):
-    num_str = str(number)
+num = input("请输入一个5位数字：")
 
-    if len(num_str) != 5:
-        return False
-    
-    return num_str == num_str[::-1]
-
-try:
-    input_num = int(input("请输入一个5位数："))
-    if (10000 <= input_num <= 99999)or(input_num == 00000):
-        if syetem_huiwen(input_num):
-            print(f"{input_num}是回文数")
-        else:
-            print(f"{input_num}不是回文数")
+if len(num) != 5 or not num.isdigit():
+    print("输入错误，必须是5位纯数字")
+else:
+    if num == num[::-1]:
+        print("是回文数")
     else:
-        print("输入错误: 请输入5位数字")
-except ValueError:
-    print("输入错误: 请输入5位数字")
+        print("不是回文数")
